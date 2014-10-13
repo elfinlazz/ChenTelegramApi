f9e5de64923b733ba025f6274d7258fa92a9fbff
BinaryTENSHi <binarytenshi@gmail.com>
2014-10-13 14:37:28 +0200

    Add console application

---
+++ src/Tests/ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using TelegramApi.MTProto.Authorization;
+using TelegramApi.MTProto.Connection;
+
+namespace TelegramApi.ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConnectionInfo connectionInfo = new ConnectionInfo("173.240.5.253", 25);
+            Authenticator authenticator = new Authenticator(new[] { connectionInfo });
+            Task attempt = authenticator.AttemptAuthentication();
+            attempt.Wait();
+        }
+    }
+}
