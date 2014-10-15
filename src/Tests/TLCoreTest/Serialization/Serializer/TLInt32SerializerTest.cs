f691e462d74c4066b125e1445c35f42d3ddecfec
BinaryTENSHi <binarytenshi@gmail.com>
2014-10-15 11:22:52 +0200

    Small code style changes.

--- src/Tests/TLCoreTest/Serialization/Serializer/TLInt32SerializerTest.cs
+++ src/Tests/TLCoreTest/Serialization/Serializer/TLInt32SerializerTest.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
-using TelegramApi.TLCore;
 using TelegramApi.TLCore.Serialization.Serializer;
 
 namespace TelegramApi.TLCoreTest.Serialization.Serializer
 {
     public class TLInt32SerializerTest
     {
-
         private TLInt32Serializer _testee;
 
         [SetUp]
@@ -21,35 +19,35 @@ namespace TelegramApi.TLCoreTest.Serialization.Serializer
         [Test]
         public void Deserialize_ThenInt32Deserialized()
         {
-            //arrange
-            Int32 expected = 4189954;
+            // arrange
+            const int Expected = 0x003FEF02;
             List<byte> byteList = new List<byte>
-            {
-                0x02,
-                0xEF,
-                0x3F,
-                0x00
-            };
+                {
+                    0x02,
+                    0xEF,
+                    0x3F,
+                    0x00
+                };
 
-            //act
-            Int32 result = (Int32) _testee.Deserialize(byteList, null);
+            // act
+            Int32 result = (Int32)_testee.Deserialize(byteList, null);
 
-            //assert
-            result.ShouldBeEquivalentTo(expected);
+            // assert
+            result.ShouldBeEquivalentTo(Expected);
             byteList.Count.ShouldBeEquivalentTo(0);
         }
 
         [Test]
         public void Serialize_ThenInt32Returned()
         {
-            //arrange
+            // arrange
             byte[] expected = { 0x02, 0xEF, 0x3F, 0x00 };
-            Int32 value = 0x3FEF02;
+            const int Value = 0x003FEF02;
 
-            //act
-            List<byte> result = _testee.Serialize(value, null);
+            // act
+            List<byte> result = _testee.Serialize(Value, null);
 
-            //assert
+            // assert
             result.ShouldBeEquivalentTo(expected);
         }
     }
