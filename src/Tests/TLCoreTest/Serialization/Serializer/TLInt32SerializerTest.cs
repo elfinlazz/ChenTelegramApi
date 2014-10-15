67d84ff1bca12278d7117428fdd1d8a7ab4e8927
Yannik Hodel <yannik.hodel@gmail.com>
2014-10-15 10:25:46 +0200

    Added Int32 Serialization Test

---
+++ src/Tests/TLCoreTest/Serialization/Serializer/TLInt32SerializerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TelegramApi.TLCore;
+using TelegramApi.TLCore.Serialization.Serializer;
+
+namespace TelegramApi.TLCoreTest.Serialization.Serializer
+{
+    public class TLInt32SerializerTest
+    {
+
+        private TLInt32Serializer _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new TLInt32Serializer();
+        }
+
+        [Test]
+        public void Deserialize_ThenInt32Deserialized()
+        {
+            //arrange
+            Int32 expected = 4189954;
+            List<byte> byteList = new List<byte>
+            {
+                0x02,
+                0xEF,
+                0x3F,
+                0x00
+            };
+
+            //act
+            Int32 result = (Int32) _testee.Deserialize(byteList, null);
+
+            //assert
+            result.ShouldBeEquivalentTo(expected);
+            byteList.Count.ShouldBeEquivalentTo(0);
+        }
+
+        [Test]
+        public void Serialize_ThenInt32Returned()
+        {
+            //arrange
+            byte[] expected = { 0x02, 0xEF, 0x3F, 0x00 };
+            Int32 value = 0x3FEF02;
+
+            //act
+            List<byte> result = _testee.Serialize(value, null);
+
+            //assert
+            result.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
